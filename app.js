b9eac39314382993e74257e723305e3d6ca667a1
mohgavin <mohammad.gavin@gmail.com>
2022-07-14 17:51:29 +0700

    Changes to be committed: 	modified:   app.js 	modified:   bin/www 	new file:   function.js 	modified:   package-lock.json 	modified:   package.json 	new file:   routes/page_error.js 	modified:   views/error.ejs 	modified:   views/index.ejs 	new file:   views/login.ejs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var page_error = require('./routes/page_error');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/page_error', page_error);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,9 +35,12 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  //render the error page
   res.status(err.status || 500);
-  res.render('error');
+  console.log(err.status);
+  res.redirect('/page_error');
+
+  //res.render('error');
 });
 
 module.exports = app;
