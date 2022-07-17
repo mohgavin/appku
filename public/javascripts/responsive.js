b69826d9873df398b7876228987b19c43084d182
mohgavin <mohammad.gavin@gmail.com>
2022-07-17 22:13:52 +0700

    Changes to be committed: 	modified:   app.js 	new file:   public/images/Untitled.png 	new file:   public/javascripts/responsive.js 	modified:   public/stylesheets/style.css 	new file:   routes/torrent.js 	modified:   views/index.ejs 	new file:   views/torrent.ejs

---
+++ public/javascripts/responsive.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    console.log("test");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+function myFunctiona() {
+    alert("welcome to Javatpoint");
+  }  
